77394d533e1f06bb4bd82d5f3be9157790a25e17
Rafkev <40280970+Rafkev@users.noreply.github.com>
2022-01-23 03:49:29 -0800

    Update share-sales-amount.php

--- share-sales-amount.php
+++ share-sales-amount.php
@@ -8,7 +8,7 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              https://coderninja.in/shivam
+ * @link              https://coderninja.ng/rafkev
  * @since             1.0.0
  * @package           Share_Sales_Amount
  *
